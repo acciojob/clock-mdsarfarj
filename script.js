46ada22360565d288f85b109c67e9110eeb3aeec
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-08-02 15:02:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function getCurrent() {
+	const now=new Date();
+	const date=now.toLocalDateString();
+	const time=now.toLocalTimeString();
+	return '${date} ${time}';
+}
+function updateTimer() {
+	const timeElement=document.getElementById('timer');
+	timerElement.textContent=getCurrent();
+	
+	
+}
+
+setInterval(updateTimer,1000);
+
+
